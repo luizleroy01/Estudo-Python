30faf4d2465483b1f0d106cb844b8eebf07f0c2c
Luiz Eduardo Leroy Souza <leroy291001@gmail.com>
2023-09-20 02:15:19 -0300

    inicio com funcoes

---
+++ Intermediario/funcoes.py
@@ -0,0 +1,43 @@
+def soma(a,b):
+    print("O resultado é ",a+b)
+
+#chamada com argumentos não nomeados
+soma(5,6)
+#chamada com argumentos nomeadados
+soma(b=76,a=534)
+
+#multiplicação com valores default
+def mult(x,y,z=None):
+    if z is not None:
+        print(x*y*z)
+    else:
+        print(x*y)
+
+mult(3,4,5)
+mult(7,6)
+
+#o valor que prevalecerá ao final será o global x
+def f1():
+    #print(x)
+    global x
+    x=10
+    print(x)
+
+    def f2():
+        #global x
+        x=15
+        print(x)
+    
+    f2()
+
+
+f1()
+
+print(x)
+
+x=200
+def f():
+    x=10
+    print(x)
+
+f()
