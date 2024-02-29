e7daf7ccabc9d99033ab0129c1082e0317fb8dd5
luizleroy01 <leroy291001@gmail.com>
2024-02-29 13:05:55 -0300

    zig zag sequence running perfectly on hacker hank

--- exerciciosEspeciais/zigZag.py
+++ exerciciosEspeciais/zigZag.py
@@ -1,20 +1,17 @@
 a.sort()
-    mid = int((n + 1)/2)
+    mid = int((n - 1)/2)
     a[mid], a[n-1] = a[n-1], a[mid]
-    
-    st = mid + 1
-    ed = n - 1
-    print(a[ed])
-    while st <= ed:
-        temp = a[ed]
-        a[ed] = a[st]
-        a[st] = temp
-        st = st + 1
-        ed = ed + 1
 
-    for i in range(n):
+    st = mid + 1
+    ed = n - 2
+    while(st <= ed):
+        a[st], a[ed] = a[ed], a[st]
+        st = st + 1
+        ed = ed - 1
+
+    for i in range (n):
         if i == n-1:
             print(a[i])
         else:
-            print(a[i], end =' ')
+            print(a[i], end = ' ')
     return
