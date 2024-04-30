e5c21af07966c9ad80b5d70f5c4eddf687c49149
luizleroy01 <leroy291001@gmail.com>
2024-04-29 23:51:18 -0300

    improving roman exercise

--- exerciciosEspeciais/romanos.py
+++ exerciciosEspeciais/romanos.py
@@ -1,5 +1,6 @@
 #exercise to interpretate roman algarisms
 letters = {'I':1,'V':5,'X':10,'L':50,'C':100,'M':1000}
+values = {'IV':4,'IX':9,'XL':40,'XC':90,'CM':900}
 
 print(letters['I'])
 print(letters['V'])
@@ -7,5 +8,6 @@ print(letters['V'])
 entrada = 'IV'
 numero = 0
 
-for i in range(len(entrada)):
-    
+for i in range(0,len(entrada),2):
+    print(entrada[i])
+        
