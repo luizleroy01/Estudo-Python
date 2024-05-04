2703f2da2285fd0a574627b0ad75b7ee8a28285d
luizleroy01 <leroy291001@gmail.com>
2024-05-03 23:48:09 -0300

    end of exercise roman coversion

--- exerciciosEspeciais/romanos.py
+++ exerciciosEspeciais/romanos.py
@@ -1,17 +1,23 @@
-#exercise to interpretate roman algarisms
-letters = {'I':1,'V':5,'X':10,'L':50,'C':100,'M':1000}
-values = {'IV':4,'IX':9,'XL':40,'XC':90,'CM':900}
+# exercise to interpretate roman algarisms
+letters = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "M": 1000}
+entrada = "MCMXLV"
 
-print(letters['I'])
-print(letters['V'])
-
-entrada = 'IV'
 numero = 0
-last = 0
+last = ''
+value = 0
 
-for simbolo in entrada:
-    if simbolo == 'I':
-        value = letters.get(simbolo)
-        numero -= value
-    elif simbolo == 'X'
-        
+for i in range(0,len(entrada)-1):
+    if entrada[i] == 'I' or entrada[i] == 'X' or entrada[i] == 'C':
+        value = letters.get(entrada[i])
+        if entrada[i+1] == 'V' or entrada[i+1] == 'L' or entrada[i+1] == 'M' or entrada[i+1] == 'X':
+            numero -= value
+        else:
+            numero += value
+    else:
+        value = letters.get(entrada[i])
+        numero += value
+
+tam = len(entrada)
+value = letters.get(entrada[tam-1])
+numero += value
+print(numero)
