ba48bb8ca75092029869d2d190d0921bea0728cc
luizleroy01 <leroy291001@gmail.com>
2024-05-12 23:52:35 -0300

    incresing tic tac toe game

--- exerciciosEspeciais/jogo_da_velha.py
+++ exerciciosEspeciais/jogo_da_velha.py
@@ -19,7 +19,21 @@ def show_table(tabuleiro):
         print()
 
 
+def game_loop(tabuleiro):
+    p1 = str("Digite a posicao da sua jogada")
+    respP1 = p1.split(" ")
+
+    linha = int(respP1[0])
+    coluna = (respP1[1])
+
+    tabuleiro[linha][coluna] = "X"
+    return tabuleiro
+
 
 game_table = create_game()
+
+show_table(game_table)
+
+game_table = game_loop(game_table)
 
 show_table(game_table)
