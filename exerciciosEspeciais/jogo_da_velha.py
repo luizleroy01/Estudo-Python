d8b9782d45616dbc127f2a14c8fd033c83b34a03
luizleroy01 <leroy291001@gmail.com>
2024-05-13 23:25:16 -0300

    round game for two players

--- exerciciosEspeciais/jogo_da_velha.py
+++ exerciciosEspeciais/jogo_da_velha.py
@@ -1,39 +1,60 @@
 def create_game():
     tabuleiro = list()
 
-    for i in range(0,3):
+    for i in range(0, 3):
         line = list()
-        for j in range(0,3):
+        for j in range(0, 3):
             line.append("*")
-        
+
         tabuleiro.append(line)
 
-    
     return tabuleiro
 
+
 def show_table(tabuleiro):
-    for i in range(0,3):
-        for j in range(0,3):
-            print(tabuleiro[i][j],end=" ")
-        
+    for i in range(0, 3):
+        for j in range(0, 3):
+            print(tabuleiro[i][j], end=" ")
+
         print()
 
 
-def game_loop(tabuleiro):
-    p1 = str("Digite a posicao da sua jogada")
-    respP1 = p1.split(" ")
+def game_loop(tabuleiro, player1):
+    if player1:
+        jogadap1 = jogada_rodada(player1)
 
-    linha = int(respP1[0])
-    coluna = (respP1[1])
+    linha = jogadap1[0]
+    coluna = jogadap1[1]
 
     tabuleiro[linha][coluna] = "X"
     return tabuleiro
 
 
+def jogada_rodada(verifyPlayer):
+    linha = int(input("Digite a linha:"))
+    coluna = int(input("Digite a coluna:"))
+    posicao_tabuleiro = linha, coluna
+
+    return posicao_tabuleiro
+
+
+def verify_table(tabuleiro):
+    disponible_positions = []
+
+    for i in range(len(tabuleiro)):
+        for j in range(len(tabuleiro[i])):
+            if tabuleiro[i][j] == "*":
+                position = i, j
+                disponible_positions.append(position)
+        
+        return disponible_positions
+
+
+isPlayer1 = False
 game_table = create_game()
 
 show_table(game_table)
 
-game_table = game_loop(game_table)
+game_table = game_loop(game_table, True)
 
 show_table(game_table)
